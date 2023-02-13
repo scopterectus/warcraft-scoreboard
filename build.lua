16b9f14ad65c37ac4046eea6c08926c79e274c9c
scopt <scopterectus@gmail.com>
2023-02-13 09:18:37 +0600

    Created.

---
+++ build.lua
@@ -0,0 +1,7 @@
+require 'build' {
+    game = "C:\\Program Files (x86)\\Warcraft III\\_retail_\\x86_64",
+    project = "C:\\Users\\scopt\\OneDrive\\warcraft-reforged\\maps\\warcraft-scoreboard",
+    map = "map.w3x",
+    src = "src",
+    run = 'game'
+}
